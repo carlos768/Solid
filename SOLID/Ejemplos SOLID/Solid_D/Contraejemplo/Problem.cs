5e9cfe980e0ebc4963a6124ec4fbdc8812cd9305
Carlos <57474308+carlos768@users.noreply.github.com>
2022-05-26 17:47:11 -0400

    Update Problem.cs

--- SOLID/Ejemplos SOLID/Solid_D/Contraejemplo/Problem.cs
+++ SOLID/Ejemplos SOLID/Solid_D/Contraejemplo/Problem.cs
@@ -2,8 +2,37 @@ class Program
 {
     public static void Main(string[] args)
     {
-        
+
     }
 
-    
+    class Shopping {
+        // Información del producto
+    }
+    // Clase de alto nivel (ShoppingBasket) depende de clases de bajo nivel
+    class ShoppingBasket
+    {
+        public void Buy(Shopping shopping)
+        {
+            SqlDatabase db = new SqlDatabase();
+            db.Save(shopping);
+            CreditCard creditCard = new CreditCard();
+            creditCard.Pay(shopping);
+        }
+    }
+    class SqlDatabase
+    {
+        public void Save(Shopping shopping)
+        {
+            Console.WriteLine("Datos guardados en la base de datos");
+        }
+    }
+    class CreditCard
+    {
+        public void Pay(Shopping shopping)
+        {
+            Console.WriteLine("Se pagó usando tarjeta de credito");
+        }
+    }
+
+    // Para cambiar el metodo de pago o base de datos, se tendría que modificar toda la la logica de ShoppingBasket 
 }
